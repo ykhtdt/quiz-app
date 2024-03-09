44bb72dfb99f5d41683a3d38e8e428f78ad6a5cb
jg <jgpark7281@gmail.com>
2024-03-10 01:28:49 +0900

    feat(api): apply quiz api route handler

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -4,17 +4,7 @@ import type { CreateQuestion } from "@/types/question";
 import { shuffle } from "@/lib/utils";
 
 const getQuestions = async (uuid: string): Promise<TriviaResponse> => {
-  const res = await fetch(
-    `${process.env.API_URL}?type=multiple&amount=10&v=${uuid}`,
-  );
-
-  if (!res.ok) {
-    if (res.status === 429) {
-      throw new Error("Too many request. Please try again");
-    }
-
-    throw new Error("Failed to fetch data");
-  }
+  const res = await fetch(`${process.env.APP_URL}/api/quiz`);
 
   return res.json();
 };
@@ -29,9 +19,9 @@ export default async function Page({ params }: { params: { hash: string } }) {
     };
   };
 
-  const questions = results.map((result) => createQuestion(result));
+  const questions = results.map(createQuestion);
 
-  console.log(questions);
+  console.log(response_code, questions);
 
   return <></>;
 }
