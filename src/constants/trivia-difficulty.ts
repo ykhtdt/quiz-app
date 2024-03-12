092c3b45fa9a3d5da5a27726f52f6fb4ab69bc5e
jgpark-1 <jgpark7281@gmail.com>
2024-03-12 20:40:46 +0900

    feat(constants): add trivia constants

---
+++ src/constants/trivia-difficulty.ts
@@ -0,0 +1,22 @@
+export const TRIVIA_DIFFICULTY = [
+  {
+    key: "0",
+    text: "Any Difficulty",
+    value: "0",
+  },
+  {
+    key: "easy",
+    text: "Easy",
+    value: "easy",
+  },
+  {
+    key: "medium",
+    text: "Medium",
+    value: "medium",
+  },
+  {
+    key: "hard",
+    text: "Hard",
+    value: "hard",
+  },
+];
---
+++ src/constants/trivia-type.ts
@@ -0,0 +1,17 @@
+export const TRIVIA_TYPE = [
+  {
+    key: "0",
+    text: "Any Type",
+    value: "0",
+  },
+  {
+    key: "multiple",
+    text: "Multiple Choice",
+    value: "multiple",
+  },
+  {
+    key: "boolean",
+    text: "True / False",
+    value: "boolean",
+  },
+];
